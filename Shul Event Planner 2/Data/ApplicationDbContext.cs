7fec79f96433ff1d4c0fdd8bd59e20db2c1e52f9
dkamins314 <42924499+dkamins314@users.noreply.github.com>
2019-02-07 10:14:15 -0600

    Add project files.

---
+++ Shul Event Planner 2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shul_Event_Planner_2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
